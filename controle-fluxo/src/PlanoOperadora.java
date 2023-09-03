b294b920f7d142753a27d1889238eed03bf8f876
CaioKWY <caiokaway@gmail.com>
2023-09-03 17:31:29 -0300

    Switch Case

---
+++ controle-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,19 @@
+public class PlanoOperadora {
+
+    public static void main(String[] args) {
+        String plano = "MIDIA";
+
+        switch(plano){
+            case "TURBO":{
+                System.out.println("5GB YouTube");
+            }
+            case "MIDIA":{
+                System.out.println("Whats e Instagram ilimitado");
+            }
+            case "BASIC":{
+                System.out.println("100 minutos ligação");
+            }
+        }
+    }
+    
+}
